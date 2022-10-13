ee4b94e3109e49a78bed8d10dc1ddc2513963036
Nancy Njeri <njerin016@gmail.com>
2022-10-13 02:00:26 -0700

    1,2,3,4,5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * #include <stdlib.h>
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
