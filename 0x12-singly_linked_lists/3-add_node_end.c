75168cabf6f165351a33de21197495aee741e2eb
Nancy Njeri <njerin016@gmail.com>
2022-10-26 04:27:03 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,7 +4,7 @@
  * add_node_end - adds a new node at the end
  * of a list_t list.
  * @head: head of the linked list.
- * @head: head of the linked list.
+ * @str: string to store in the list.
  * Return: address of the head.
  */
 
