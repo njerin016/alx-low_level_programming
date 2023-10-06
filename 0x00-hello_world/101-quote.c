01c39483d0f3487dd03d3ba41aa39114b2f8336b
Nancy <njerin016@gmail.com>
2023-10-06 07:16:14 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19, followed by a new line
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
